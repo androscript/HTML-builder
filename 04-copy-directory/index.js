66719f3de4df946227aa06d469a064c1fceaad1b
Androscript <androscript@gmail.com>
2021-11-07 21:42:40 +0200

    feat: add solution for task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,49 @@
 
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs/promises');
+
+
+const oldFolder = path.join(__dirname, 'files');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+const newFolder = path.join(__dirname, 'files-copy');
+
+fs.readdir(oldFolder, {withFileTypes: true}, (err, files) => {
+  if (err) throw err;
+  for (let i = 0; i < files.length; i++) {
+    if (files[i].isFile()) {
+      fsPromises.copyFile(path.join(oldFolder, files[i].name), path.join(newFolder, files[i].name));
+    }
+  }
+})
+
+fsPromises.readdir(newFolder, {withFileTypes: true}).then((arr) => {
+  const newFolderFiles = [];
+  for (let item of arr) {
+    if (item.isFile()) {
+      newFolderFiles.push(item.name);
+    }
+  }
+
+  fsPromises.readdir(oldFolder, {withFileTypes: true}).then((arr) => {
+    const oldFolderFiles = [];
+    for (let item of arr) {
+      if (item.isFile()) {
+        oldFolderFiles.push(item.name);
+      }
+    }
+
+    for (let item of newFolderFiles) {
+      if (!oldFolderFiles.includes(item)) {
+        fsPromises.rm(path.join(newFolder, item));
+      }
+    }
+
+  })
+
+})
+
